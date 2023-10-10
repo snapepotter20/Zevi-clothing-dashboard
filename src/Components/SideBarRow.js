24ad086b689a18004c7d5600d2f5091dadb33120
Sahil Kumar <reavensnape07@gmail.com>
2023-10-11 00:25:33 +0530

    sidebarrow updated

--- src/Components/SideBarRow.js
+++ src/Components/SideBarRow.js
@@ -42,45 +42,12 @@ const SideBarRow = ({ Icon, title }) => {
 
   return (
     <div className={"sidebarRow"}>
-      {/* <Icon className="sidebarRow__icon" />
-      {title === "*****"
-        ? (title = (
-            <Rating
-              size="small"
-              icon={<StarRateIcon></StarRateIcon>}
-              value={5}
-              precision={0.5}
-              style={{ marginLeft: "2px" }}
-              readOnly
-            />
-          ))
-        : title === "****"
-        ? (title = (
-            <Rating
-              size="small"
-              icon={<StarRateIcon></StarRateIcon>}
-              value={4}
-              precision={0.5}
-              style={{ marginLeft: "2px" }}
-              readOnly
-            />
-          ))
-        : title === "***"
-        ? (title = (
-            <Rating
-              size="small"
-              icon={<StarRateIcon></StarRateIcon>}
-              value={3}
-              precision={0.5}
-              style={{ marginLeft: "2px" }}
-              readOnly
-            />
-          ))
-        :     
-        null}
-      <p className="sidebarRow__title">{title}</p> */}
       {Icon && <Icon className="sidebarRow__icon" />}
-      {title==='*****' || title==='****' || title === '***' ? ratingComponent: <p className="sidebarRow__title">{title}</p> }
+      {title === "*****" || title === "****" || title === "***" ? (
+        ratingComponent
+      ) : (
+        <p className="sidebarRow__title">{title}</p>
+      )}
     </div>
   );
 };
